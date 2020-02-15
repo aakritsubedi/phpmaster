b76a336338c2becd410038ae2496d6166c2db239
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-02-15 11:46:46 +0545

    Day4: Add CRUD SQL queries

--- day4/introSQL.php
+++ day4/introSQL.php
@@ -36,12 +36,13 @@
     <hr>
     <ul class="code">
       <li>SELECT 
+        <span class="def">The <code>SELECT</code> statement is used to select data from a database.The data returned is stored in a result table, called the result-set.</span>
         <pre>
           <code>
           SELECT column1, column2, ... FROM table_name;
           </code>
         </pre>
-        Examples
+        Examples:
         <span class="example">SELECT * FROM students;</span>
         <span class="example">SELECT student_name, contact_no FROM students;</span>
         <span class="example">SELECT DISTINCT column1, column2, ... FROM table_name;</span>
@@ -50,13 +51,45 @@
         <span class="example">SELECT column1, column2, ... FROM table_name ORDER BY column1, column2, ... ASC|DESC;</span>
       </li>
       <li>INSERT
+        <span class="def">The <code>INSERT INTO</code> statement is used to insert new records in a table.</span>
         <pre>
           <code>
           INSERT INTO table_name VALUES (value1, value2, value3, ...);
           </code>
         </pre>
+        Examples:
+        <span class="example">INSERT INTO student (student_name, address, roll_no, email) VALUES ('Aakrit Subedi', 'Basundhara', '45/BCT/072', 'aakritsubedi9@gmail.com');</span>
+        <span class="example">INSERT INTO student (student_name) VALUES ('Aakrit Subedi');</span>
+      </li>
+      <li>UPDATE
+        <span class="def">The <code>UPDATE</code> statement is used to modify the existing records in a table.</span>
+        <pre>
+          <code>
+          UPDATE table_name
+          SET column1 = value1, column2 = value2, ...
+          WHERE condition;
+          </code>
+        </pre>
+        Examples:
+        <span class="example">UPDATE student SET student_name = 'KP Oli' WHERE roll_no = '25/CSIT/075';</span>
+        <span class="example">UPDATE student SET student_name = 'Raj Oli', City= 'Jhapa' WHERE roll_no = '25/CSIT/075';</span>
+        <span class="example">UPDATE student SET college = 'Sagarmatha Engineering College' WHERE address = 'Sanepa';</span>
+        <span class="example">UPDATE student SET address = 'Sanepa'; //update all record</span>
+      </li>
+      <li>DELETE
+        <span class="def">The <code>DELETE</code> statement is used to delete existing records in a table.</span>
+        <pre>
+          <code>
+          DELETE FROM table_name WHERE condition;
+          </code>
+        </pre>
+        Examples: 
+        <span class="example">DELETE FROM student WHERE student_name='Aakrit Subedi';</span>
+        <span class="example">DELETE FROM table_name; //delete all records</span>
       </li>
     </ul>
+    <a href="https://www.w3schools.com/sql/default.asp">More SQL queries</a>
+    <hr>
   </div>
 </body>
 
